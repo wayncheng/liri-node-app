7a4e765384395e5178fdaeaaccb4d0b2c3aab0d4
wayne <wayne@che.ng>
2017-06-22 02:51:30 -0700

    cleaned up

--- liri.js
+++ liri.js
@@ -8,7 +8,8 @@
 	var fs = require('fs');
 	// console.log('keys',keys);
 
-	var op = process.argv[2];
+	// var op = process.argv[2].trim().replace('-','_');
+	var op = process.argv[2].trim();
 	var query = '';
 	for (var i=3; i<process.argv.length; i++) {
 		query = query + ' ' + process.argv[i];
@@ -16,24 +17,6 @@
 	query = query.trim();
 
 	execute(op);
-	// console.log('op',op);
-
-	// var commands = {
-	// 	'my-tweets': function(){
-	// 	},
-	// 	'spotify-this-song': function(){
-
-	// 	},
-	// 	'movie-this': function(){
-
-	// 	},
-	// 	'do-what-it-says': function(){
-
-	// 	},
-	// }
-
-	// commands[op];
-
 	function execute(op){
 		switch(op){
 			case 'my-tweets':
@@ -78,11 +61,9 @@
 		    logOut('\n--------------------------------------------\n');
 		  }
 		});
-		// var screen_name = 'wayncheng';
-		// var qURL = `https://api.twitter.com/1.1/statuses/user_timeline.json?${screen_name}=twitterapi&count=20`;
 		
-		
-	}
+	};
+
 	function spotify(){
 		var queryType = 'track';
 
@@ -100,39 +81,18 @@
 		},function(err, data) {
 		    if (err) {return console.log('Error occurred: ' + err); }
 
-		    // console.log(data);
-		    // var items = data.tracks.items;
-		    // console.log('items[0]',items[0]);
 		    var r = data.tracks.items[0];
 		    var artists = r.artists[0].name;
 		    var songname = r.name;
 		    var previewURL = r.preview_url;
 		    var album = r.album.name;
 
-	    	// var logs = [
-			   //  ' | Artist: '+ artists,
-			   //  ' | Song: '+ songname,
-			   //  ' | Album: '+ album,
-			   //  ' | Preview URL: '+ previewURL,
-	    	// ];
-
-	    	// console.log('Result =================================');
-
 	    	var out = `\n============================================\nSpotify Response for "${query}"\n | Artist: ${artists}\n | Song: ${songname}\n | Album: ${album}\n | Preview URL: ${previewURL}\n----------------------------------------\n`;
 	    	logOut(out);
-	    	// console.log(out);
-	    	// for (var i=0; i<logs.length; i++) {
-	    		// console.log(logs[i])
-	    	// };
-		    // console.log(' | Artist:',artists);
-		    // console.log(' | Song:',songname);
-		    // console.log(' | Album:',album);
-		    // console.log(' | Preview URL:',previewURL);
-	    	// console.log(' ----------------------------------------');
   			
 		});
 
-	}
+	};
 
 	function movie(){
 		if (query.length == 0){
@@ -161,20 +121,9 @@
 
 	    	var out = `\n============================================\nOMDB Response for "${query}"\n | Title: ${b.Title}\n | Year: ${b.Year}\n | Rating: ${imdbRating}\n | Country: ${b.Country}\n | Language: ${b.Language}\n | Plot: ${b.Plot}\n | Actors: ${b.Actors}\n | Website: ${b.Website}\n----------------------------------------\n`;
 
-	    	// console.log(out);
 	    	logOut(out);
-	    	// console.log('Result =================================');
-		    // console.log(' | Title:',b.Title);
-		    // console.log(' | Year:', b.Year);
-		    // console.log(' | IMDB Rating:', imdbRating);
-		    // console.log(' | Country:', b.Country);
-		    // console.log(' | Language:', b.Language);
-		    // console.log(' | Plot:', b.Plot);
-		    // console.log(' | Actors:', b.Actors);
-		    // console.log(' | Website:', b.Website);
-	    	// console.log(' ----------------------------------------');
 		});
-	}
+	};
 
 
 
@@ -189,26 +138,14 @@
 			}
 		})
 
-	}
+	};
 
 	function logOut(out){
-		// for (var i=0; i<logArray.length; i++) {
-		// 	fs.appendFile('log.txt', '\n'+logArray[i],function(err,data){
-		// 		if (err) { return console.log(err) }
-		// 		else {
-		// 			// console.log('logOut success')
-		// 		}
-		// 	})
-		// };
 		console.log(out);
 
 		fs.appendFile('log.txt',out,function(err,data){
 				if (err) { return console.log(err) }
-				// else {
-					// console.log('logOut success')
-				// }
-
 		})
-	}
+	};
 
 })();
